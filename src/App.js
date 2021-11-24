2208ed14beda5363eb8510db4cdaa9380a6e32ed
Yannis <yannis.barba.dev@gmail.com>
2021-11-24 17:30:04 +0100

    add a main div and a flex grow 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
 import StarIcon from '@mui/icons-material/Star';
 
 function App() {
   return (
     <div id="App">
-      Hackathon
-      <StarIcon />
+      <header> Header</header>
+      <div id="main">
+        Hackathon
+        <StarIcon />
+      </div>
+      <footer>Footer</footer>
     </div>
   );
 }
