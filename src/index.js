6cfe986f4959aa61765f79359445bee8fd99aa88
Yannis <yannis.barba.dev@gmail.com>
2021-11-25 16:39:27 +0100

    remove AppChat from index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import AppChat from './components/firebase/AppChat';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    <AppChat />
   </BrowserRouter>,
   document.getElementById('root')
 );
