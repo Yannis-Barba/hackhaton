e96fb6ea12e804fa2bca7c5ded724ed5f307c23e
Dr Wouse <e.gon66@outlook.fr>
2021-11-25 18:44:24 +0100

    Integration Profile Ok

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Avatar } from '@mui/material';
+import Divid from '../components/Divid';
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div className="flex flex-col justify-between items-center">
+        <h1 className="text-third font-bold text-3xl mt-4"> PROFILE </h1>
+        <Avatar
+          className="mt-10"
+          alt="Avatar"
+          src="./assets/yb.png"
+          sx={{ width: 70, height: 70, border: '#5A2C81' }}
+        />
+        <h3 className="font-semibold text-third mt-3"> Yannis Legénie </h3>
+        <p className=" text-sm m-7 text-justify text-third">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+      </div>
+      <Divid />
+    </div>
+  );
 }
 
 export default Profile;
